a6fefc2edeb7460ff940ac9d1c2146c228c61260
Jared Cohen <jaredecohen17@gmail.com>
2021-12-02 16:33:35 -0500

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
-var db = require("./database.js")
+var db = require('./database.js')
 
 // Require md5 MODULE
 var md5 = require("md5")
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
-HTTP_PORT = 5000;
+var HTTP_PORT = 5000;
 
 // Start server
 app.listen(HTTP_PORT, () => {
