c94ccca8652cd38d85997e7d011dffc8ed34391f
Jared Cohen <jaredecohen17@gmail.com>
2021-12-02 20:29:19 -0500

    endpoints

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-	const info = stmt.run(req.params.id)
-	res.status(200).json(stmt);
+	const info = stmt.run(req.body.user,req.body.pass, req.params.id);
+	res.status(200).json({"message":"1 record updated: ID 2 (200)"});
 })
 
 
